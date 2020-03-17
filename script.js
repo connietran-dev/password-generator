b70184a2fbbcad780fb222164add031e47593cf7
Connie Tran <connietrandev@gmail.com>
2020-03-16 22:27:36 -0400

    Declare JS variables for form fields and adjust HTML accordingly

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+// Create an empty object to store the form values.
+var formValues = {};
+
+// If a checkbox is unchecked when its form is submitted, there is no value submitted to the server to represent its unchecked state (e.g. value=unchecked); the value is not submitted to the server at all.
+// However, if it is checked when a form is submitted, the data name/value pair will be submitted - subscribe=newsletter.
+
+
+formValues.pwLength = document.getElementById("pwLength").value;
+formValues.inclSymbols = document.getElementById("inclSymbols").value;
+formValues.inclNumbers = document.getElementById("inclNumbers").value;
+formValues.inclLowerCase = document.getElementById("inclLowerCase").value;
+formValues.inclUpperCase = document.getElementById("inclUpperCase").value;
+
+console.log(formValues);
+
+// form.lastName = document.getElementById('lastName').value;
+
+
+
+
 // Assignment Code
 // document.querySelector() returns the first Element within the document that matches the specified selector. 
 // In this case, "Generate Password" button is returned and stored in generateBtn variable.
