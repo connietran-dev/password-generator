f054f68e50bcc85ffe9087327c2e91d91f6491bb
Connie Tran <connietrandev@gmail.com>
2020-03-16 21:54:27 -0400

    Add comments on base Javascript

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 // Assignment Code
+// document.querySelector() returns the first Element within the document that matches the specified selector. 
+// In this case, "Generate Password" button is returned and stored in generateBtn variable.
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
+  
+  // generatePassword() function does not yet exist.
+  // Once it does exist, it will generate a password and store it in the 'password' variable.
   var password = generatePassword();
+
+  // Returns the <textarea> when "Your Secure Password" and stores it in the variable 'passwordText'.
   var passwordText = document.querySelector("#password");
 
+  // Takes 'password' that is generated from generatePassword() and stores it as the value for 'passwordText'
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
+// When the "Generate Password" button (generateBtn) is clicked, the writePassword() function will be called.
 generateBtn.addEventListener("click", writePassword);
